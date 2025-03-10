5d62b91a633c6903421f3d08a6cdd6b1528944ec
not-ad-chaos <chaotic10101@gmail.com>
2025-03-10 16:22:06 +0530

    ...

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main(void) {
   vector<int> universe = {20, 30, -10, 2};
-  cout << "COME ONNN WORKKK! \n";
   cout << "The answer to life, universe and everything is: "
        << accumulate(universe.cbegin(), universe.cend(), 0) << endl;
   cout << "-- Douglas Adams" << endl;
