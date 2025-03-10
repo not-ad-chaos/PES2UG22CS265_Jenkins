d52a9f17ba3951ec8090fab0b981c02d2fdca700
not-ad-chaos <chaotic10101@gmail.com>
2025-03-10 15:26:49 +0530

    the only correct way

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,14 @@
-//File: hello.cpp
+// File: hello.cpp
+#include <numeric>
 #include <iostream>
 using namespace std;
 
 int main(void) {
+  std::vector<int> universe = {20, 30, -10, 2};
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "The answer to life, universe and everything is: "
+       << accumulate(universe.cbegin(), universe.cend(), 0) << endl;
   return 0;
 }
