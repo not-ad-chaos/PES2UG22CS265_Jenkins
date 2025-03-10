03252f7f2d13d5d744422f7756c03a47ba5ab520
not-ad-chaos <chaotic10101@gmail.com>
2025-03-10 16:09:56 +0530

    work this time?

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 int main(void) {
   vector<int> universe = {20, 30, -10, 2};
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
   cout << "The answer to life, universe and everything is: "
        << accumulate(universe.cbegin(), universe.cend(), 0) << endl;
   cout << "-- Douglas Adams" << endl;
